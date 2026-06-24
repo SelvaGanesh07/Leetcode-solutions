9e3c3363931b65e7e527eed0afcbda46aa83c745
SelvaGanesh <lselvaganesh12@gmail.com>
2026-06-24 21:03:46 +0530

    Added merge strings

---
+++ Strings/1768_Merge_Strings_Alternately.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        ans = ""
+        n = min(len(word1), len(word2))
+        for i in range(n):
+            ans += word1[i]
+            ans += word2[i]
+        ans += word1[n:]
+        ans += word2[n:]
+        return ans
